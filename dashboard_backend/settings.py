0f314ac9cd04bbf0ecdc0850bbf2871358616922
Daniel <sotdan@users.noreply.github.com>
2021-03-19 14:31:32 +0100

    signup and refactoring

--- dashboard_backend/settings.py
+++ dashboard_backend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'assessment.apps.AssessmentConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
