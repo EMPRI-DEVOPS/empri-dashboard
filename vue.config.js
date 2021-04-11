397e0f4c517308eb735b85c4219ab0735f788b02
Daniel <sotdan@users.noreply.github.com>
2021-04-11 16:20:34 +0200

    fix for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/static/dist/' : 'http://127.0.0.1:8080',
+    publicPath: process.env.NODE_ENV === 'production' ? '/static/' : 'http://127.0.0.1:8080',
     outputDir: 'static',
     indexPath: '../templates/base-vue.html', // relative to outputDir!
 
