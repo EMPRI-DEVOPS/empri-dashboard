6fde5eaf25163546dcf4972f73a45bb7a85d55b4
Daniel <sotdan@users.noreply.github.com>
2021-04-23 18:37:35 +0200

    404 page

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="col-md-12 text-center">
+    <span class="display-1 d-block">404</span>
+    <div class="mb-4 lead">The page you are looking for was not found.</div>
+    <router-link to="/" class="btn btn-link">Back to Home</router-link>
+  </div>
+</template>
