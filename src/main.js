6c8c592772a2ce7d531232071e53afa41f2825d6
Daniel <sotdan@users.noreply.github.com>
2021-04-25 19:45:19 +0200

    improved css bundling

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { createApp } from 'vue'
+import {
+    createApp
+} from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/app.css";
 
 createApp({})
-.use(router)
-.use(VueAxios, axios)
-.mount('#app')
+    .use(router)
+    .use(VueAxios, axios)
+    .mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,20 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/static/' : 'http://127.0.0.1:8080',
     outputDir: 'static',
     indexPath: '../templates/base-vue.html', // relative to outputDir!
+    filenameHashing: false,
 
     runtimeCompiler: true,
     // von hier: https://github.com/EugeneDae/django-vue-cli-webpack-demo/blob/master/client/vue.config.js
-    chainWebpack: config => {
-        config.devServer
-            .public('http://127.0.0.1:8080')
-            .hotOnly(true)
-            .headers({
-                "Access-Control-Allow-Origin": "*"
-            })
-            .writeToDisk(filePath => filePath.endsWith('index.html'));
+    devServer: {
+        writeToDisk: filePath => filePath.endsWith('index.html') || filePath.endsWith('.css')
+    },
+    css: {
+        extract: true,
+        sourceMap: true,
+        loaderOptions: {
+            css: {
+                // options here will be passed to css-loader
+            },
+        }
     }
 }
