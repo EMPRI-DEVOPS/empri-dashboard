65eb57b5df7fa48843568cedcbda4234eb8dad39
Daniel <sotdan@users.noreply.github.com>
2021-04-27 20:21:10 +0200

    better login/signup pages

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 
 
@@ -10,5 +11,13 @@ class IndexTemplateView(TemplateView):
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('entry-point')
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        valid = super().form_valid(form)
+        username, password = form.cleaned_data.get(
+            'username'), form.cleaned_data.get('password1')
+        new_user = authenticate(username=username, password=password)
+        login(self.request, new_user)
+        return valid
